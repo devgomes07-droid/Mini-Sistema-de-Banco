3da2fa48aba49f910b36f95fda7ecdf35321e7ba
dev.gomes <devgomes07@gmail.com>
2025-12-31 21:40:26 -0300

    Ajusta logica de deposito e saque e melhora validacoes

--- src/Conta.java
+++ src/Conta.java
@@ -1,34 +1,34 @@
 import java.util.ArrayList;
 
 public class Conta {
+
     private double saldo;
-    private ArrayList <String> historico;
+    private ArrayList<String> historico;
 
     public Conta() {
         saldo = 0;
         historico = new ArrayList<>();
     }
-
-    public void depositar(double valor) {
-        if (valor > 0) {
-            saldo = saldo + valor;
-            historico.add("Depósito: " + valor);
+    public boolean depositar(double valor) {
+        if (valor <= 0) {
+            return false;
         }
+        saldo += valor;
+        historico.add("Depósito: " + valor);
+        return true;
     }
     public boolean sacar(double valor) {
-        if (valor > 0 && valor <= saldo) {
-            saldo -= valor;
+        if (valor <= 0 || valor > saldo) {
+            return false;
+        }
+        saldo -= valor;
         historico.add("Saque: " + valor);
         return true;
-        }
-         return false;
     }
     public double getSaldo() {
         return saldo;
     }
-
     public ArrayList<String> getHistorico() {
         return new ArrayList<>(historico);
     }
 }
-
--- src/Main.java
+++ src/Main.java
@@ -7,68 +7,69 @@ public class Main {
         Conta conta = new Conta();
 
         while (true) {
-            // Menu
             System.out.println("\n1 - Depositar");
             System.out.println("2 - Sacar");
             System.out.println("3 - Ver Saldo");
             System.out.println("4 - Ver Histórico");
             System.out.println("5 - Sair");
-            System.out.println("Escolha:");
+            System.out.print("Escolha: ");
 
             if (!sc.hasNextInt()) {
-                System.out.println("Entrada inválida, digite um número.");
+                System.out.println("Entrada inválida.");
                 sc.next();
                 continue;
             }
 
             int opcao = sc.nextInt();
-
             switch (opcao) {
-                case 1: {
-                    System.out.print("Valor: ");
+                case 1:
+                    System.out.print("Valor do depósito: ");
                     if (sc.hasNextDouble()) {
                         double valor = sc.nextDouble();
-                        if (valor > 0) {
-                            conta.depositar(valor);
-                            System.out.println("Depósito realizado!");
+                        if (conta.depositar(valor)) {
+                            System.out.println("Depósito realizado com sucesso!");
                         } else {
-                            System.out.println("Valor inválido.");
+                            System.out.println("Valor inválido para depósito.");
                         }
                     } else {
-                        System.out.println("Valor inválido.");
+                        System.out.println("Entrada inválida.");
                         sc.next();
                     }
                     break;
-                }
-                case 2: {
-                    System.out.print("Valor: ");
+
+                case 2:
+                    System.out.print("Valor do saque: ");
                     if (sc.hasNextDouble()) {
                         double valor = sc.nextDouble();
                         if (conta.sacar(valor)) {
                             System.out.println("Saque realizado com sucesso!");
                         } else {
-                            System.out.println("Saque não realizado, saldo insuficiente.");
+                            System.out.println("Saque não realizado. Verifique o saldo.");
                         }
                     } else {
-                        System.out.println("Valor inválido.");
+                        System.out.println("Entrada inválida.");
                         sc.next();
                     }
                     break;
-                }
+
                 case 3:
-                    System.out.println("Saldo: " + conta.getSaldo());
+                    System.out.println("Saldo atual: " + conta.getSaldo());
                     break;
+
                 case 4:
                     System.out.println("Histórico:");
                     System.out.println(conta.getHistorico());
                     break;
+
                 case 5:
                     System.out.println("Saindo...");
                     sc.close();
                     return;
+
                 default:
-                    System.out.println("Opção inválida!");
+                    System.out.println("Opção inválida.");
             }
         }
     }
 }
+
