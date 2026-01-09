5a69367a1e67aabd154effed0101536557eee089
dev.gomes <devgomes07@gmail.com>
2026-01-09 15:08:26 -0300

    feat(model): adiciona classe Agencia com lista de contas

--- src/model/Agencia.java
+++ src/model/Agencia.java
@@ -1,4 +1,63 @@
 package model;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Agencia {
+
+ //   === ATRIBUTOS ===
+
+    private String numero;
+    private String nome;
+    private List<Conta> contas;
+
+//    === CONSTRUTOR ===
+
+    public  Agencia(String numero,String nome) {
+        this.numero = numero;
+        this.nome = nome;
+        this.contas = new ArrayList<>();
+    }
+
+//    === METODOS ===
+
+//adicionar uma conta à agência
+
+    public void adicionarConta(Conta conta) {
+        if (conta != null && !contas.contains(conta)) {
+            contas.add(conta);
+            System.out.println("Conta de " + conta.getNomeCompleto() + " adicionada à agência " + nome);
+        } else {
+            System.out.println("Conta invalída ou já existente");
+        }
+    }
+
+//Lista todas as contas da agência
+
+    public void listarContas() {
+        System.out.println("=== Contas da Agência " + nome + " ===");
+        if (contas.isEmpty()) {
+            System.out.println("Nenhuma conta cadastrada");
+            return;
+        }
+        for (Conta c : contas) {
+            System.out.println(c.getNomeCompleto() + " | Saldo: R$ " + c.getSaldo());
+        }
+    }
+//Busca uma conta pelo nome completo
+
+    public Conta buscarConta(String nomeCompleto) {
+        for (Conta c : contas) {
+            if (c.getNomeCompleto().equalsIgnoreCase(nomeCompleto)) {
+                return c;
+            }
+        }
+        return null; //retorna null se não achar
+    }
+
+//Getters básicos
+
+    public String getNumero() {return numero;}
+    public String getNome() { return nome;}
+    public List<Conta> getContas() { return new ArrayList<>();}
+
 }
